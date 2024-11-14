37fae7550bc65627eb3dfaefca0126fdec37b83b
NoahCSC <ncastevens117@gmail.com>
2024-11-14 16:11:53 -0500

    Finished Tutorial08

---
+++ homework/hw04/src/components/Bookmark.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import {postDataToServer, deleteDataFromServer} from "../server-requests";
+export default function Bookmark({token, bookmarkId, postId}){
+    const [stateBookmarkId, setStateBookmarkId] = useState(bookmarkId);
+async function createBookmark() {
+    const sendData = {
+        "post_id":postId,
+    }
+    console.log("Creating bookmark");
+    const responseData = await postDataToServer(token, "/api/bookmarks/", sendData);
+    console.log(responseData);
+    setStateBookmarkId(responseData.id);
+}
+async function deleteBookmark() {
+    const url='/api/bookmarks/21'+ stateBookmarkId;
+    console.log("Deleting bookmark");
+    const responseData = await deleteDataFromServer(token, url);
+    console.log(responseData);
+    setStateBookmarkId(null);
+}
+    if(stateBookmarkId){
+    return (<button onClick={deleteBookmark}><i className="fas fa-bookmark"></i></button>);
+    } else {
+        return (<button onClick={createBookmark}><i className="far fa-bookmark"></i></button>);
+    }
+}
